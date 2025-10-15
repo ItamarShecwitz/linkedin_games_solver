9e5a0dbe66599222a912ca3a4afcdce28377f196
Itamars <TheWaterPaper11@gmail.com>
2025-10-15 13:22:12 +0300

    working

--- scripts/content.js
+++ scripts/content.js
@@ -2,6 +2,8 @@ const BOARD_SIZE = 6;
 const SUN_NAME = "Sun";
 const MOON_NAME = "Moon";
 const EMPTY_NAME = "Empty";
+const CROSS_NAME = "Cross";
+const EQUAL_NAME = "Equal";
 const NUMBER_OF_DIRECTIONS = 4;
 const ZERO = 0;
 
@@ -12,6 +14,7 @@ class Cell {
         this.index = index;
         this.row =  Math.floor(index / BOARD_SIZE);
         this.column = index % BOARD_SIZE;
+        this.action = [];
     }
 
     lock_cell() {
@@ -23,42 +26,69 @@ class Cell {
     }
 }
 
-
 function getRawBoard(document) {
-    return document.querySelector('.lotka-grid.gil__grid');
+    return document.querySelector('.lotka-grid');
 }
 
 function createBoard(board_raw) {
     let cell;
+    let edge;
     let svg;
     let title;
     let board = [];
     let index;
+    let sign;
+    let locked;
 
     if (!board_raw) { return null; }
     for (let child of board_raw.children) {
         // if(cell = child.querySelector('.lotka-cell-content.lotka-cell-content--locked')){
         if(cell = child.querySelector('.lotka-cell-content')){
             if(svg = cell.querySelector('svg')){
-                index = child.getAttribute('data-cell-idx');
-                board[index] = new Cell(svg.getAttribute("aria-label"), true, index);
-                console.log(svg.getAttribute("aria-label") + " in: " + child.getAttribute('data-cell-idx'));
+                index = parseInt(child.getAttribute('data-cell-idx'));
+                sign = svg.getAttribute("aria-label");
+                locked = (sign == EMPTY_NAME ? false : true)
+                board[index] = new Cell(sign, locked, index);
+                console.log(sign + " in: " + index);
             }
         }
+
     }
     return board;
 }
 
-// function checkBoardValidity(board){
-//     let row = 0;
-//     let column = 0;
-// }
+function set_action(board_raw, board){
+    let action;
+    let action_neighbor;
+
+    if (!board_raw) { return null; }
+    for (let child of board_raw.children) {
+        if(edge = child.querySelector('.lotka-cell-edge')){
+            if(svg = edge.querySelector('svg')){
+                action = svg.getAttribute("aria-label");
+                index = parseInt(child.getAttribute('data-cell-idx'));
+                board[index].action_neighbor = -1;
+                if(edge.classList.contains('lotka-cell-edge--right')){
+                    action_neighbor = index + 1;
+                    
+                } else if(edge.classList.contains('lotka-cell-edge--down')){
+                    action_neighbor = index + BOARD_SIZE;
+                }
+                console.log(action_neighbor + " x " + index );
+                
+                board[index].action.push([action, action_neighbor]);
+                board[action_neighbor].action.push([action, index]);
+            } 
+        }
+    }   
+}
 
 function checkRowValidity(board, row){
     let sun_count = 0, moon_count = 0;
 
     for(let i = 0; i < BOARD_SIZE; i++){
-        switch(board[row + i].sign){
+
+        switch(board[row * BOARD_SIZE + i].sign){
             case SUN_NAME:
                 sun_count++;
                 break;
@@ -75,7 +105,7 @@ function checkColumnValidity(board, column){
     let sun_count = 0, moon_count = 0;
 
     for(let i = 0; i < BOARD_SIZE; i++){
-        switch(board[column + i*6].sign){
+        switch(board[column + i*BOARD_SIZE].sign){
             case SUN_NAME:
                 sun_count++;
                 break;
@@ -105,25 +135,110 @@ function checkAdjacent(board, index){
         if(board[index + 1].sign == sign && board[index - 1].sign == sign) { return false;}
     }
 
-    if(row + 2 < BOARD_SIZE){ 
-        if(board[index + 1 * BOARD_SIZE].sign == sign && board[index - 1 * BOARD_SIZE].sign == sign) { return false;}
+    if(row + 2 < BOARD_SIZE){
+if (!board_raw) { return null; }        if(board[index + 1 * BOARD_SIZE].sign == sign && board[index + 2 * BOARD_SIZE].sign == sign) { return false;}
     }
     if(row - 2 > ZERO){ 
-        if(board[index + 1 * BOARD_SIZE].sign == sign && board[index + 2 * BOARD_SIZE].sign == sign) { return false;}
+        if(board[index - 1 * BOARD_SIZE].sign == sign && board[index - 2 * BOARD_SIZE].sign == sign) { return false;}
     }
     if(row + 1 < BOARD_SIZE && row - 1 > ZERO){ 
-        if(board[index - 1 * BOARD_SIZE].sign == sign && board[index - 2 * BOARD_SIZE].sign == sign) { return false;}
+        if(board[index + 1 * BOARD_SIZE].sign == sign && board[index - 1 * BOARD_SIZE].sign == sign) { return false;}
     }
 
     return true;
 }
 
+function checkAction(board, index){
+    if(!board[index].action) {return true;}
+    let action;
+    let action_neighbor;
+    for (let act of board[index].action){
+        action = act[0];
+        action_neighbor = act[1];
+        if(board[index].sign == EMPTY_NAME || board[action_neighbor].sign == EMPTY_NAME) {continue;}
 
-const board_raw = getRawBoard(document);
+        if(action == CROSS_NAME){
+            if (board[index].sign == board[action_neighbor].sign) {return false;}
+        }
+        if(action == EQUAL_NAME){
+            if (board[index].sign != board[action_neighbor].sign) {return false;}
+        }
+    }
+
+    return true;
+}
+
+function checkCellValidity(board, index){
+    return (checkRowValidity(board, board[index].row) && checkColumnValidity(board, board[index].column) && checkAdjacent(board, index) && checkAction(board, index));
+}
+
+function backtracking(board, index, sign){
+    if(index >= 36){
+        return board;
+    }
+
+    if(!board[index].locked){
+        board[index].sign = sign;
+    }
+    
+    let new_board = null;
+    
+    if(checkCellValidity(board, index)){
+
+        new_board = backtracking(board, index + 1, MOON_NAME);
+        if(new_board == null){
+            new_board = backtracking(board, index + 1, SUN_NAME);
+        }
+        if(new_board == null && !board[index + 1].locked){
+            board[index + 1].sign = EMPTY_NAME;
+        }
+    }
+    return new_board;
+
+}
+
+function dispatchClick(el) {
+    const down = new MouseEvent('mousedown', { bubbles: true, cancelable: true, view: window });
+    const up = new MouseEvent('mouseup', { bubbles: true, cancelable: true, view: window });
+    const click = new MouseEvent('click', { bubbles: true, cancelable: true, view: window });
+    el.dispatchEvent(down);
+    el.dispatchEvent(up);
+    el.dispatchEvent(click);
+}
+
+function play_board(board_raw, board){
+    for (let child of board_raw.children) {
+        if(cell = child.querySelector('.lotka-cell-content')){
+            index = parseInt(child.getAttribute('data-cell-idx'));
+            if(board[index].sign == SUN_NAME){
+                dispatchClick(cell);
+            } else if(board[index].sign == MOON_NAME){
+                dispatchClick(cell);
+                dispatchClick(cell);
+            }
+        }
+    }  
+}
+
+
+
+const iframe = document.querySelector('.game-launch-page__iframe');
+const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+const board_raw = iframeDoc.querySelector('.lotka-grid');
+// const board_raw = document.querySelector('.lotka-grid');
+
 // board.style.display = 'none';
-let board = createBoard(board_raw);
+console.log(board_raw);
 
+let board = createBoard(board_raw);
+set_action(board_raw, board);
 if(checkRowValidity(board, 0)){console.log("Yay");} else {console.log("Nay");}
 if(checkColumnValidity(board, 2)){console.log("Yay2");} else {console.log("Nay2");}
 if(checkAdjacent(board, 21)){console.log("Yay3");} else {console.log("Nay3");}
 
+new_board = backtracking(board, 0, SUN_NAME);
+console.log(new_board);
+
+if(checkRowValidity(new_board, 1)){console.log("Yay3");} else {console.log("Nay3");}
+play_board(board_raw, board);
